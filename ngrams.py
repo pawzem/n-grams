534028f80999a9560465744bc8fea99f8ebf8ae6
Paweł Żemła <pawzem@gmail.com>
2019-05-04 13:24:06 +0200

    character count

--- ngrams.py
+++ ngrams.py
@@ -1,5 +1,6 @@
 import sys
 import nltk
+from collections import Counter
 
 
 def main():
@@ -8,17 +9,36 @@ def main():
     file = open(file_name, "rt")
 
     lines = file.readlines()
-    trigrams(lines)
+    ngrams_analysis(lines)
 
     file.close()
 
 
-def trigrams(lines):
-    tokens = []
+def ngrams_analysis(lines):
+    text = ''
     for line in lines:
-        tokens.append(nltk.word_tokenize(line))
-    print(list(nltk.trigrams(tokens)))
-#     bigrams also avilible
+        text += line
+    tokens = nltk.word_tokenize(text)
+    trigrams = nltk.trigrams(tokens)
+    bigrams = nltk.bigrams(tokens)
+    print("digrams\n")
+    analize(bigrams)
+    print("trigrams\n")
+    analize(trigrams)
+    print("character count\n")
+    analize_characters(text)
+
+
+def analize_characters(text):
+    counts = Counter(text)
+    for character, count in counts.items():
+        print("'{}' {}".format(character, count))
+
+
+def analize(ngrams):
+    dist = nltk.FreqDist(ngrams)
+    for ngram, count in dist.items():
+        print(ngram, count)
 
 
 if __name__ == "__main__":
