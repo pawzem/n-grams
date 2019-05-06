bde8f786f9768ecda0614ef7aaeb05d4af43807b
Paweł Żemła <pawzem@gmail.com>
2019-05-06 20:38:19 +0200

    languages

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # n-grams
 project which will find digrams and trigrams of given text 
-
+fr - http://www.gutenberg.org/cache/epub/58501/pg58501.txt
+de- http://www.gutenberg.org/cache/epub/39277/pg39277.txt
+cz- https://manybooks.net/titles/zapisky-z-mrtveho-domu?ga_submit=lrf:wYPn083k0jJtKgu
+hun- https://manybooks.net/titles/petofi-sandor-osszes-koltemenyei
 
 requires nltk.download('punkt')
--- ngrams.py
+++ ngrams.py
@@ -1,15 +1,21 @@
 import sys
 import nltk
+import json
 from collections import Counter
 
 
 def main():
     print("performing analysis of digrams, trigrams and special chars\n")
-    file_name = sys.argv[1]
-    file = open(file_name, "rt")
+    file_name = sys.argv[2]
+    file = open(file_name, "rt", encoding="utf8")
 
     lines = file.readlines()
-    ngrams_analysis(lines)
+    analysis = ngrams_analysis(lines)
+    analysis['language'] = sys.argv[1]
+
+    output_file = open(sys.argv[3], 'wt', encoding="utf8")
+    json_dumps = json.dumps(analysis)
+    output_file.write(json_dumps)
 
     file.close()
 
@@ -19,26 +25,31 @@ def ngrams_analysis(lines):
     for line in lines:
         text += line
     tokens = nltk.word_tokenize(text)
+
     trigrams = nltk.trigrams(tokens)
     bigrams = nltk.bigrams(tokens)
-    print("digrams\n")
-    analize(bigrams)
-    print("trigrams\n")
-    analize(trigrams)
-    print("character count\n")
-    analize_characters(text)
+
+    digrams = analize(bigrams, len(text))
+    trigrams = analize(trigrams, len(text))
+    characters = analize_characters(text)
+
+    return {'digrams': digrams, 'trigrams': trigrams, 'characters': characters}
 
 
 def analize_characters(text):
+    result = {}
     counts = Counter(text)
     for character, count in counts.items():
-        print("'{}' {}".format(character, count))
+        result[character] = count / len(text)
+    return result
 
 
-def analize(ngrams):
+def analize(ngrams, length):
+    result = {}
     dist = nltk.FreqDist(ngrams)
     for ngram, count in dist.items():
-        print(ngram, count)
+        result["{}".format(ngram)] = count / length
+    return result
 
 
 if __name__ == "__main__":
