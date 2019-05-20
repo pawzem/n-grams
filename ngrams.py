f8d8fb25fe4db46c41007b5642effd8fb4d29f78
Paweł Żemła <pawzem@gmail.com>
2019-05-20 21:03:04 +0200

    german text analysis

--- ngrams.py
+++ ngrams.py
@@ -2,22 +2,62 @@ import sys
 import nltk
 import json
 from collections import Counter
+import matplotlib.pyplot as plt
 
 
 def main():
+    if sys.argv[1] == 'analise':
+        analise_text()
+    else:
+        generate_model()
+
+
+def analise_text():
+    print("analysing given text\n")
+    reference_model_location = sys.argv[2]
+    reference_model_file = open(reference_model_location, "r", encoding="utf8")
+    reference_model = json.load(reference_model_file)
+    input_model_location = sys.argv[3]
+    input_model = prepare_model_analysis(input_model_location)
+    plot_from_model(input_model, reference_model, 'digrams')
+    plot_from_model(input_model, reference_model, 'trigrams')
+    plot_from_model(input_model, reference_model, 'characters')
+
+
+def plot_from_model(input_model, reference_model, model_type):
+    ngram_output_reference_dict = {}
+    ngram_output_input_dict = {}
+    for input_key in input_model[model_type].keys():
+        if input_key in reference_model[model_type].keys():
+            ngram_output_reference_dict[input_key] = reference_model[model_type][input_key]
+            ngram_output_input_dict[input_key] = input_model[model_type][input_key]
+    plt.plot(list(ngram_output_reference_dict.keys()), list(ngram_output_reference_dict.values()), 'r--',
+             label='reference model')
+    plt.plot(list(ngram_output_input_dict.keys()), list(ngram_output_input_dict.values()), 'b--', label='input model')
+    plt.xlabel('tokens')
+    plt.ylabel('frequency')
+    plt.title(model_type)
+    plt.gca().legend(('reference model', 'input model'))
+    plt.show()
+
+
+def generate_model():
     print("performing analysis of digrams, trigrams and special chars\n")
     file_name = sys.argv[2]
-    file = open(file_name, "rt", encoding="utf8")
+    analysis = prepare_model_analysis(file_name)
+    json_dumps = json.dumps(analysis)
+    output_file = open(sys.argv[3], 'wt', encoding="utf8")
+    output_file.write(json_dumps)
 
+
+def prepare_model_analysis(file_name):
+    file = open(file_name, "rt", encoding="utf8")
     lines = file.readlines()
+    file.close()
     analysis = ngrams_analysis(lines)
     analysis['language'] = sys.argv[1]
 
-    output_file = open(sys.argv[3], 'wt', encoding="utf8")
-    json_dumps = json.dumps(analysis)
-    output_file.write(json_dumps)
-
-    file.close()
+    return analysis
 
 
 def ngrams_analysis(lines):
